2b97a80e725360d6873a681640f941ac9e181ce8
byu46 <ihpkaillus@gmail.com>
2021-04-23 01:09:54 -0700

    init

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace byu46
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.byu46.ColourblindSettings", "ColourblindSettings", "1.0.0")]
+    public class ColourblindSettings : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var newTier2ItemColour = new UnityEngine.Color32(102, byte.MaxValue, byte.MaxValue, byte.MaxValue);
+            var newTier2ItemDarkColour = new UnityEngine.Color32(8, 149, 149, byte.MaxValue);
+            var newEquipmentItemColour = new UnityEngine.Color32(225, 164, 191, byte.MaxValue);
+            Logger.LogMessage("Loaded ColourblindSettings!");
+
+            On.RoR2.ColorCatalog.GetColor += (orig, self) =>
+            {
+                switch (self)
+                {
+                    case ColorCatalog.ColorIndex.Tier2Item:
+                        return newTier2ItemColour;
+                    case ColorCatalog.ColorIndex.Equipment:
+                        return newEquipmentItemColour;
+                    case ColorCatalog.ColorIndex.Tier2ItemDark:
+                        return newTier2ItemDarkColour;
+                    default:
+                        return orig(self);
+                }
+            };
+
+            On.RoR2.ColorCatalog.GetColorHexString += (orig, self) =>
+            {
+                switch (self)
+                {
+                    case ColorCatalog.ColorIndex.Tier2Item:
+                        return Util.RGBToHex(newTier2ItemColour);
+                    case ColorCatalog.ColorIndex.Equipment:
+                        return Util.RGBToHex(newEquipmentItemColour);
+                    case ColorCatalog.ColorIndex.Tier2ItemDark:
+                        return Util.RGBToHex(newTier2ItemDarkColour);
+                    default:
+                        return orig(self);
+                }
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ror2-colourblind-settings
+# This is a mod for Risk of Rain 2 designed to help colourblind players distinguish between different item types more easily.
+
+My plan is to add options to customize all item colours individually per the user's preference.
